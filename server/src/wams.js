6b7b86f1fe20ebb1569a881d0c3170a9ac6ab77b
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-02-11 10:36:14 -0400

    feat,refactor(wams): add send,broadcast on server; create calls objects

--- server/src/wams.js
+++ server/src/wams.js
@@ -3,6 +3,30 @@ var socket_io = require('socket.io')
   , User = require('./user')
   ;
 
+//All events that sent to server
+/**
+ *
+ * @type {{new_connection: string, subscribe_mt_event: string, send_message: string, broadcast_message: string}}
+ */
+var server_io_recv_calls = {
+   new_connection:       "CONN"
+   , subscribe_mt_event: "MT_EVENT_SUBSCRIBE"
+   , send_message:       "SEND_MSG"
+   , broadcast_message:  "SEND_BROADCAST"
+};
+//All events that received from server
+/**
+ *
+ * @type {{connection_ok: string, user_connected: string, user_disconnected: string, receive_message: string, receive_broadcast: string}}
+ */
+var server_io_send_calls = {
+   connection_ok:        "CONN_OK"
+   , user_connected:     "CONN_USER"
+   , user_disconnected:  "DEL_USER"
+   , receive_message:    "RECV_MSG"
+   , receive_broadcast:  "RECV_BROADCAST"
+};
+
 var MTEvents = [
    "hold", "tap", "doubletap", "drag", "dragstart", "dragend", "dragup",
    "dragdown", "dragleft", "dragright", "swipe", "swipeup", "swipedown",
@@ -24,36 +48,62 @@ exports.listen = function(server, options, callback) {
    var io = socket_io.listen(server, options, callback);
 
    io.sockets.on('connection', function(socket) {
-      socket.on('CONN', function(data) {
-         var newUser = new User(getUUID(),"", data.description);
+      socket.on(server_io_recv_calls.new_connection, function(data) {
+         var newUser = new User(getUUID(), socket, "", data.description);
          users.push(newUser);
 
-         socket.emit('CONN_OK', {
             uuid: newUser.uuid,
             otherClients: users.except(newUser)
+         socket.emit(server_io_send_calls.connection_ok, {
          });
 
-         socket.on('MT_EVENT_SUBSCRIBE', function(data) {
+         socket.on(server_io_recv_calls.subscribe_mt_event, function(data) {
             data.eventType.split(" ").forEach(function(type) {
                newUser.subscribe(type);
                socket.join(type);
             });
          });
 
-         socket.broadcast.emit('CONN_USER', {
             client: newUser
+         socket.broadcast.emit(server_io_send_calls.user_connected, {
          });
 
          MTEvents.forEach(function(type) {
             socket.on(type, function(data) {
-               console.log(data);
-               socket.broadcast.to(type).emit(type, data);
+               socket.broadcast.to(type).emit(type, {
+                  source: newUser.uuid,
+                  data: data
+               });
+            });
+         });
+
+         socket.on(server_io_recv_calls.send_message, function(data) {
+            var source = data.source;
+            var targets = data.targets;
+            var msg = data.msg;
+
+            targets.forEach(function(target) {
+               var user = users.get(target);
+               user.socket.emit(server_io_send_calls.receive_message, {
+                  source: source,
+                  data: msg
+               });
+            });
+         });
+
+         socket.on(server_io_recv_calls.broadcast_message, function(data) {
+            var source = data.source;
+            var msg = data.msg;
+
+            socket.broadcast.emit(server_io_send_calls.receive_broadcast, {
+               source: source,
+               data: msg
             });
          });
 
          socket.once('disconnect', function() {
-            socket.broadcast.emit('DEL_USER', {
                client: newUser
+            socket.broadcast.emit(server_io_send_calls.user_disconnected, {
             });
             users.pop(newUser);
          });
