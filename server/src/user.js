201c83061c8ca4591f3dc21a624c551e63b4679a
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-02-11 10:21:35 -0400

    feat(user): store socket with user object
    Move methods to prototype of user.
    Add method to copy significant fields when copy user object to send over
    socket.

--- server/src/user.js
+++ server/src/user.js
@@ -1,16 +1,26 @@
-var user = function(uuid, mode, description) {
+var user = function(uuid, socket, mode, description) {
    this.uuid = uuid;
+   this.socket = socket;
    this.mode = mode;
    this.description = description;
    this.MTSubscription = [];
+};
 
-   this.equals = function(user) {
-      return (this.uuid === user.uuid);
-   };
+user.prototype.equals = function(user) {
+   return (this.uuid === user.uuid);
+};
 
-   this.subscribe = function(event) {
-      this.MTSubscription.push(event);
-   };
+user.prototype.subscribe = function(event) {
+   this.MTSubscription.push(event);
+};
+
+user.prototype.copy = function() {
+   var result = {};
+
+   result.uuid = this.uuid;
+   result.description = this.description;
+
+   return result;
 };
 
 module.exports = user;
