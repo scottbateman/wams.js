040b397a731752c315da9d6a1282f8d9cfdd3135
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-03-25 12:25:00 -0300

    feat(major): start shared workspace example

---
+++ examples/sharedWorkspace/app.js
@@ -0,0 +1,54 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var os = require('os');
+var wams = require('wams.js-server');
+
+var app = express();
+
+var iface = os.networkInterfaces().wlp3s0;
+var IP;
+iface && iface.forEach(function(connection) {
+   if (connection.family === 'IPv4') {
+      IP = connection.address;
+   }
+});
+
+// all environments
+app.set('ip', process.argv[2] || process.env.IP || IP || '127.0.0.1');
+app.set('port', process.argv[3] || process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+// app.set('env', 'development');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' === app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+function renderRoot(req, res) {
+   res.render('sharedWorkspace', { title: 'Shared space between clients' });
+}
+
+app.get('/', renderRoot);
+
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on ' + app.get('ip') + ':' + app.get('port'));
+});
+
+wams.listen(server);
+
+
---
+++ examples/sharedWorkspace/public/js/sharedWorkspace.js
@@ -0,0 +1,16 @@
+var rndColor = function() {
+   var bg_colour = Math.floor(Math.random() * 16777215).toString(16);
+   bg_colour = "#"+("000000" + bg_colour).slice(-6);
+   return bg_colour;
+};
+var sampleNames = [
+   'Fe', 'Thomas', 'Kirstie', 'Wynell', 'Mario', 'Aretha', 'Cherryl', 'Ta',
+   'Lindy', 'Karina', 'Sacha', 'Latesha', 'Miki', 'Janel', 'Leola', 'Romeo',
+   'Roderick', 'Felica', 'Ilona', 'Nila', 'Patrina', 'Wes', 'Henry', 'Elvera',
+   'Karrie', 'Jacklyn', 'Alethea', 'Emogene', 'Alphonso', 'Chandra', 'Beryl',
+   'Lilly', 'Georgetta', 'Darrin', 'Deane', 'Rocio', 'Charissa', 'Simona',
+   'Don', 'Arianne', 'Esther', 'Leonia', 'Karma', 'Rosemarie', 'Carolyn',
+   'Miriam', 'Chastity', 'Vesta', 'Christian', 'Lashaun'
+].sort();
+var name = sampleNames[Math.floor(Math.random() * sampleNames.length)];
+var color = rndColor();
