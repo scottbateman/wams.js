b2dcaaced1de359e39496e89d8c65c61e1efc0a7
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-04-03 10:23:25 -0300

    docs(workspace): add docs to methods of Workspace class

--- examples/sharedWorkspace/workspace.js
+++ examples/sharedWorkspace/workspace.js
@@ -1,3 +1,9 @@
+/**
+ * Create new workspace
+ * @param {String} uuid UUID of workspace
+ * @param {{width: number, height: number, x: number, y: number, scale: number}} screen Screen parameters
+ * @constructor
+ */
 var Workspace = function(uuid, screen) {
    this.uuid = uuid;
    if (typeof screen === 'string') {
@@ -6,6 +12,11 @@ var Workspace = function(uuid, screen) {
    this.screen = screen;
 };
 
+/**
+ * Compares to workspaces by UUID
+ * @param {String|Workspace} workspace UUID or Workspace that we want to compare to
+ * @returns {boolean} True if UUIDs are equal
+ */
 Workspace.prototype.equals = function(workspace) {
    if (typeof workspace === 'string') {
       return this.uuid === workspace;
@@ -14,12 +25,22 @@ Workspace.prototype.equals = function(workspace) {
    }
 };
 
+/**
+ * Encode screen to simplified string to send over network
+ * @param {{width: number, height: number, x: number, y: number, scale: number}} screen Screen parameters
+ * @returns {string} Simplified string
+ */
 Workspace.encode = function(screen) {
    return screen.width + ':' + screen.height + ':' +
       screen.x + ':' + screen.y + ':' +
       screen.scale;
 };
 
+/**
+ * Decode networked screen into useful object
+ * @param {String} str Simplified string
+ * @returns {{width: number, height: number, x: number, y: number, scale: number}} Decoded object
+ */
 Workspace.decode = function(str) {
    var splittedStr = str.split(':');
 
