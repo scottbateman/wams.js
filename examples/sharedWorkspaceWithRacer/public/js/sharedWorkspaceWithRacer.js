70ccca822ecf0773039dcbae0bfb51055fce787d
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-05-23 10:48:21 -0300

    feat(clone): function to deep clone objects and arrays

--- examples/sharedWorkspaceWithRacer/public/js/sharedWorkspaceWithRacer.js
+++ examples/sharedWorkspaceWithRacer/public/js/sharedWorkspaceWithRacer.js
@@ -21,6 +21,31 @@ var rndName = (function() {
    return sampleNames[Math.floor(Math.random() * sampleNames.length)];
 }());
 
+function clone(obj) {
+   // Handle the 2 simple types, and null or undefined
+   if (null == obj || "object" != typeof obj) return obj;
+
+   // Handle Array
+   if (obj instanceof Array) {
+      var copy = [];
+      for (var i = 0, len = obj.length; i < len; i++) {
+         copy[i] = clone(obj[i]);
+      }
+      return copy;
+   }
+
+   // Handle Object
+   if (obj instanceof Object) {
+      var copy = {};
+      for (var attr in obj) {
+         if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+      }
+      return copy;
+   }
+
+   throw new Error("Unable to copy obj! Its type isn't supported.");
+}
+
 function displayScreenMode(screen) {
    var screenMode = $('#mode');
    screenMode.text(screen.w + 'x' + screen.h +
