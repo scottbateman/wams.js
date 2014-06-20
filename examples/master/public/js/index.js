ff5af9b04bd5f833a0b0456d4391124b611b5769
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-06-20 22:16:56 +0400

    feat(client): create all links to examples
    Whenever page is loaded, take ports of each example and create direct
    link to this example.

--- examples/master/public/js/index.js
+++ examples/master/public/js/index.js
@@ -1,1 +1,13 @@
 var socket = io();
+
+$(document).ready(function() {
+   var port, link, links = $('.link a');
+
+   links.each(function() {
+      link = $(this);
+      port = link.attr('data-port');
+
+      link.attr('href',  'http://' + location.hostname + ':' + port + '/');
+      link.text(location.hostname + ':' + port);
+   });
+});
