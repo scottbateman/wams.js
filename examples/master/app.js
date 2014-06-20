c2c814a69c386fc443447f7bf44c32f50729f88b
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-06-20 21:40:37 +0400

    feat(clone): provide function to deep clone objects

--- examples/master/app.js
+++ examples/master/app.js
@@ -7,6 +7,32 @@ var http = require('http'),
    favicon = require('static-favicon'),
    logger = require('morgan');
 
+function clone(obj) {
+   // Handle the 2 simple types, and null or undefined
+   if (null === obj || "object" !== typeof obj) { return obj; }
+
+   var copy, i, len, attr;
+   // Handle Array
+   if (obj instanceof Array) {
+      copy = [];
+      for (i = 0, len = obj.length; i < len; i++) {
+         copy[i] = clone(obj[i]);
+      }
+      return copy;
+   }
+
+   // Handle Object
+   if (obj instanceof Object) {
+      copy = {};
+      for (attr in obj) {
+         if (obj.hasOwnProperty(attr)) { copy[attr] = clone(obj[attr]); }
+      }
+      return copy;
+   }
+
+   throw new Error("Unable to copy obj! Its type isn't supported.");
+}
+
 /**
  * all fields in EXAMPLE_LIST object
  * {
